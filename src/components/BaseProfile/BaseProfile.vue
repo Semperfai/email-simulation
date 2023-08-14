84f814e98c5009617b8d1ecd2d853227c0a57471
Semperfai <svyatoslavmatyuha@gmail.com>
2023-08-14 14:30:51 +0300

    chore: update BaseProfile

--- src/components/BaseProfile/BaseProfile.vue
+++ src/components/BaseProfile/BaseProfile.vue
@@ -1,32 +1,55 @@
 <template>
   <div id="BaseProfile">
     <img
+      @click="openMenu = !openMenu"
       id="tooltip-trigger-user"
       class="rounded-full w-8 cursor-pointer"
       src="https://via.placeholder.com/40"
       alt="avatar"
     />
     <div
+      v-show="!openMenu"
       id="tooltip-content-user"
       role="tooltip"
-      class="inline-block absolute invisible text-xs z-10 py-1 px-2 font-medium text-white rounded-sm shadow-sm opacity-0 tooltip bg-gray-600 delay-150"
+      class="inline-block absolute invisible text-xs z-10 py-1 px-2 font-medium text-white rounded-sm shadow-sm tooltip bg-gray-600"
     >
       <div>Email Account</div>
-      <div class="text-gray-300">BEBRA MEN</div>
-      <div class="text-gray-300">sadasdas@gmail.com</div>
+      <div class="text-gray-300">Igor Dorin</div>
+      <div class="text-gray-300">testmail@gmail.com</div>
+    </div>
+    <div
+      v-show="openMenu"
+      class="absolute z-10 w-80 right-2 bg-white top-14 rounded-lg custom-shadow"
+    >
+      <div class="w-full flex justify-center">
+        <img class="rounded-full w-20 mt-4" src="https://via.placeholder.com/40" />
+      </div>
+      <div class="text-gray-700 w-full flex justify-center mt-2 text-lg">Igor Dorin</div>
+      <div class="text-gray-700 w-full flex justify-center text-sm pb-4 border-b">
+        testmail@gmail.com
+      </div>
+      <div class="flex justify-center my-5">
+        <button
+          class="bg-transparent text-xs hover:bg-gray-100 text-gray-600 font-semibold py-2 px-4 border border-gray-300 rounded"
+        >
+          Sign out of Email
+        </button>
+      </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { Tooltip, type TooltipOptions } from 'flowbite'
 
+const openMenu = ref<boolean>(false)
+
 onMounted(() => {
-  const $targetEl = document.getElementById(`tooltip-content-user`)
-  const $triggerEl = document.getElementById(`tooltip-trigger-user`)
+  const $targetEl = document.getElementById(`tooltip-content-user`) as HTMLElement
+  const $triggerEl = document.getElementById(`tooltip-trigger-user`) as HTMLElement
 
   const options: TooltipOptions = {
-    placement: 'auto',
+    placement: 'bottom',
     triggerType: 'hover'
   }
 
@@ -35,4 +58,12 @@ onMounted(() => {
   }
 })
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#BaseProfile {
+  .custom-shadow {
+    box-shadow: -1px 1px 6px -2px rgba(36, 36, 36, 0.58);
+    -webkit-box-shadow: -1px 1px 6px -2px rgba(36, 36, 36, 0.58);
+    -moz-box-shadow: -1px 1px 6px -2px rgba(36, 36, 36, 0.58);
+  }
+}
+</style>
