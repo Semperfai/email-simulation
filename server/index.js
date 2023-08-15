dfd57d1c75819722a74ccdcd20c56b5dd856e41e
Semperfai <svyatoslavmatyuha@gmail.com>
2023-08-15 11:44:14 +0300

    feat: create server route

---
+++ server/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const { OAuth2Client } = require('google-auth-library')
+
+const client = new OAuth2Client(
+  '565535016155-qcvpltqcqm7l8fl6f5puo9gmmsaqqm0r.apps.googleusercontent.com'
+)
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/api/google-login', async (req, res) => {
+  const ticket = await client.verifyIdToken({
+    idToken: req.body.tokenId
+  })
+
+  res.status(200).json(ticket.getPayload())
+})
+
+app.listen(4001, () => console.log('Server started on port 4001'))
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import MessagePage from '../pages/MessagePage.vue'
 import LoginPage from '../pages/LoginPage.vue'
 
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    component: LoginPage
+  },
+  {
+    path: '/email',
+    name: 'email',
+    component: () => import('../pages/template/EmailPage.vue'),
+    children: [
+      {
+        path: '',
+        name: 'home-page',
+        component: HomePage
+      },
+      {
+        path: 'message/:id',
+        component: MessagePage
+      }
+    ]
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      component: LoginPage
-    },
-    {
-      path: '/email',
-      name: 'email',
-      component: () => import('../pages/template/EmailPage.vue'),
-      children: [
-        {
-          path: '',
-          name: 'home-page',
-          component: HomePage
-        },
-        {
-          path: 'message/:id',
-          component: MessagePage
-        }
-      ]
-    }
-  ]
+  routes
 })
 
 export default router
